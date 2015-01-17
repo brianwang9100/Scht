fb16d64d92850a932ee62cc48c4c85b448b6ab70
bigwangsmash <bigwangsmash@0587364508.wireless.umich.net>
2015-01-16 23:30:51 -0500

    first commit

---
+++ Scht/Scht/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Scht
+//
+//  Created by Brian Wang on 1/16/15.
+//  Copyright (c) 2015 Scht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Scht/Scht/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Scht
+//
+//  Created by Brian Wang on 1/16/15.
+//  Copyright (c) 2015 Scht. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
